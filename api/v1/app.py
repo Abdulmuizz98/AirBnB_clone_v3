08faed084d97b72282fca1dcbf061106e2df5ae7
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-10-23 20:38:02 +0100

    make executable

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,12 +17,14 @@ def tear_down(exception):
     """Release Resources"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
-    if  (request.path.startswith('/api/v1/states/') and 
-         request.method == 'POST'):
+    if (request.path.startswith('/api/v1/states/') and
+       request.method == 'POST'):
         return ('Not a JSON')
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     hst = getenv('HBNB_API_HOST')
