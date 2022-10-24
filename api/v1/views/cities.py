c65db9fee7824eb3f5e8d312fb15978f82694180
felixokolo <pheelix.info@gmail.com>
2022-10-24 10:30:27 +0100

    correction

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -49,7 +49,7 @@ def cities_post(state_id):
         new_city.save()
         return (jsonify(new_city.to_dict()), 201)
 
-@app_views.route('/states/<city_id>/cities', methods=['PUT'])
+@app_views.route('/cities/<city_id>', methods=['PUT'])
 def cities_put(city_id):
     city = storage.get(City, city_id)
     if city is None:
@@ -61,46 +61,5 @@ def cities_put(city_id):
         for k, v in city_dict.items():
             if k not in ['id', 'state_id', 'created_at', 'updated_at']:
                 setattr(city, k, v)
-            state.save()
-            return (jsonify(state.to_dict()), 200)
-# @app_views.route('/states/', methods=['GET', 'POST'])
-# @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
-# def states_get(state_id=None):
-    # """Returns states in storage"""
-    # if state_id is None:
-        # if request.method == 'GET':
-            # states_dict = [v.to_dict() for k, v in storage.all(State).items()]
-            # return jsonify(states_dict)
-        # elif request.method == 'POST':
-            # state_dict = request.get_json()
-            # if not request.get_json():
-                # return make_response(jsonify({'error': 'Not a JSON'}), 400)
-            # else:
-                # if 'name' not in state_dict:
-                    # return make_response(jsonify({'error': 'Missing name'}), 400)
-                # new_state = State(**state_dict)
-                # new_state.save()
-                # return make_response(jsonify(new_state.to_dict())), 201
-    # else:
-        # state = storage.get(State, state_id)
-        # if state is None:
-            # abort(404)
-            # return
-        # if request.method == 'GET':
-            # return (jsonify(state.to_dict()))
-        # if request.method == 'DELETE':
-            # storage.delete(state)
-            # storage.save()
-            # return (jsonify({}))
-        # if request.method == 'PUT':
-            # state_dict = request.get_json()
-            # if state_dict is None:
-                # abort(404)
-            # else:
-                # if (state_dict.get('name', None)) is None:
-                    # abort(404)
-                # for k, v in state_dict.items():
-                    # if k not in ['id', 'created_at', 'updated_at']:
-                        # setattr(state, k, v)
-                    # state.save()
-                # return (jsonify(state.to_dict()), 200)
+            city.save()
+            return (jsonify(city.to_dict()), 200)
