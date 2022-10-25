e4863583b9d933c492668d8df54acc4bc9348555
felixokolo <pheelix.info@gmail.com>
2022-10-25 09:41:17 +0100

    correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,7 +22,7 @@ def tear_down(exception):
 @app.errorhandler(404)
 def not_found(error):
     """Not found"""
-    return make_response(jsonify([{'error': 'Not found'}]), 404)
+    return make_response(jsonify("[{'error': 'Not found'}]"), 404)
 
 
 if __name__ == "__main__":
