3c37257ce7cb87efa1704dd20e02f25d8d694037
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-12-16 13:31:25 +0100

    done

--- models/user.py
+++ models/user.py
@@ -14,7 +14,7 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column("Password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
@@ -30,10 +30,11 @@ class User(BaseModel, Base):
         if kwargs:
             # first time user creation hash password
             if not kwargs.get("id", None) and kwargs.get("password", None):
-                self._password = hashlib.md5(kwargs["password"].encode()).hexdigest()
+                kwargs["password"] = hashlib.md5(kwargs["password"].encode()).hexdigest()
+            """
             if kwargs.get("id", None) and kwargs.get("password", None):
                 self._password = kwargs["password"]
-
+            """
         super().__init__(*args, **kwargs)
 
     @property
